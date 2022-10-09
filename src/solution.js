0288b8711b8f0db03951967b82773906afe4242e
apurvk4 <apurvkumar4299@gmail.com>
2022-10-09 11:47:40 +0530

    updated url

--- src/solution.js
+++ src/solution.js
@@ -1,16 +1,22 @@
 const solution = async (language, code, input) => {
+  let b = {
+    language,
+    code,
+  };
+  if (input) {
+    b["input"] = input;
+  }
   try {
-    const response = await fetch(process.env.REACT_APP_FETCHURL, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        language,
-        code,
-        input,
-      }),
-    });
+    const response = await fetch(
+      "https://online-compiler-backend11.herokuapp.com/code",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(b),
+      }
+    );
     return response.json();
   } catch (e) {
     return { result: e.message };
